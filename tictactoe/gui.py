75fea5b5e9ba635d07bad672f1f5165c60ae40ac
ab2i <brzoska.ale@gmail.com>
2024-07-15 15:15:25 +0200

    Colored results and fixed tab bug

--- tictactoe/gui.py
+++ tictactoe/gui.py
@@ -1,17 +1,26 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk, messagebox
 from tictactoe_logic import gameloop, reset_array, current_player
 
 def make_gui():
     root = tk.Tk()
     root.geometry('300x300')
     root.title('TicTacToe')
+
+    style = ttk.Style()
+    style.configure('X.TButton', background='green')
+    style.configure('O.TButton', background='red')
+    style.configure('Tie.TButton', background='blue')
+
     label = ttk.Label(root, text='Turn: X')
     label.configure(anchor="center")
-    label.grid(sticky='NSEW', row=4, column= 1)
+    label.grid(sticky='NSEW', row=4, column=1)
+
     buttons = make_buttongrid(root, label)
+
     reset_button = ttk.Button(root, text='Reset', command=lambda: reset_board(root, buttons, label))
-    reset_button.grid(sticky='NSEW', row=4, column= 2)
+    reset_button.grid(sticky='NSEW', row=4, column=2)
+
     root.mainloop()
 
 def make_buttongrid(root, label):
@@ -23,7 +32,7 @@ def make_buttongrid(root, label):
             root.grid_columnconfigure(col, weight=1)
             make_button(root, row, col, buttons, label)
     return buttons
-            
+
 def make_button(root, row, col, buttons, label):
     button = ttk.Button(root)
     button.grid(row=row, column=col, sticky='NSEW')
@@ -39,6 +48,7 @@ def reset_board(root, buttons, label):
     for row in buttons:
         for btn in row:
             btn.destroy()
-    make_buttongrid(root, label)
+    buttons = make_buttongrid(root, label)
     reset_array()
     label['text'] = 'Turn: X'
+    buttons[0][0].focus()
--- tictactoe/tictactoe_logic.py
+++ tictactoe/tictactoe_logic.py
@@ -20,28 +20,30 @@ def update_board(board, user_input, new_value, buttons):
   board[row][col] = new_value
   buttons[row][col]['text'] = new_value
 
-def check_win(board, buttons):
+def check_win(board):
     # Horizontal
     for row in range(3):
-       if board[row][0] == board[row][1] == board[row][2] != '0':
-           return True
+        if board[row][0] == board[row][1] == board[row][2] != '0':
+            return True, [(row, 0), (row, 1), (row, 2)]
     # Vertical
     for col in range(3):
-       if board[0][col] == board[1][col] == board[2][col] != '0': 
-           return True
+        if board[0][col] == board[1][col] == board[2][col] != '0':
+            return True, [(0, col), (1, col), (2, col)]
     # Diagonal
-    if board[0][0] == board[1][1] == board[2][2] != '0': 
-        return True
-    if board[2][0] == board[1][1] == board[0][2] != '0': 
-        return True
+    if board[0][0] == board[1][1] == board[2][2] != '0':
+        return True, [(0, 0), (1, 1), (2, 2)]
+    if board[2][0] == board[1][1] == board[0][2] != '0':
+        return True, [(2, 0), (1, 1), (0, 2)]
 
     count = 9
     for row in range(3):
         for col in range(3):
-            if board[row][col] == 'X' or board[row][col] == 'O': count -= 1
+            if board[row][col] == 'X' or board[row][col] == 'O':
+                count -= 1
     if count < 1:
-        return 'Tie'
-    return False
+        return 'Tie', []
+
+    return False, []
 
 def make_turn(board, index, buttons):
     user_choice = index
@@ -53,8 +55,13 @@ def gameloop(r, c, buttons, label):
     global current_player
     index = 3 * r + c + 1
     make_turn(board, index, buttons)
-    check = check_win(board, buttons)
+    check, positions = check_win(board)
     if check == True:
+        for pos in positions:
+            if current_player:
+                buttons[pos[0]][pos[1]].config(style='X.TButton')
+            else:
+                buttons[pos[0]][pos[1]].config(style='O.TButton')
         for row in buttons:
             for btn in row:
                 btn.state(['disabled'])
@@ -66,15 +73,13 @@ def gameloop(r, c, buttons, label):
         for row in buttons:
             for btn in row:
                 btn.state(['disabled'])
+                btn.config(style='Tie.TButton')
         string = 'Game is tied!'
         print(string, '\n')
         label['text'] = string
         messagebox.showinfo(message=string)
     else:
-        if label['text'] == 'Turn: X':
-            label['text'] = 'Turn: O'
-        else:
-            label['text'] = 'Turn: X'
+        label['text'] = 'Turn: ' + ('O' if current_player else 'X')
         current_player = not current_player
 
 print()
